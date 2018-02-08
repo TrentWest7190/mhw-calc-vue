2d56246b0663561b23073f9ca09dea1bde628b60
Trent West <Trent.West@motivedrilling.com>
2018-02-08 14:30:24 -0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <WeaponSelector></WeaponSelector>
+  </div>
+</template>
+
+<script>
+import WeaponSelector from './components/WeaponSelector'
+
+export default {
+  name: 'App',
+  components: {
+    WeaponSelector
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#app {
+
+}
+</style>
---
+++ src/components/WeaponSelector.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style style="scss" scoped>
+.container {
+    border: 1px solid red;
+    border-radius: 5px;
+    width: 100px;
+    height: 100px;
+}
+
+</style>
